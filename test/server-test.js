5912630af52caba6da24f82e2527c30290b7cf17
Alireza Andersen <alireza.andersen@gmail.com>
2016-07-15 22:32:00 -0600

    created static index

--- test/server-test.js
+++ test/server-test.js
@@ -1,22 +1,34 @@
 const assert = require('assert');
+const request = require('request');
 const app = require('../server');
 
-before(done => {
-  this.port = 9876;
-  this.server = app.listen(this.port, (err, result) => {
-    if (err) { return done(err); }
-    done();
-  });
-});
-
-after(() => {
-  this.server.close();
-});
-
 describe('Server', () => {
+
+  before((done) => {
+    this.port = 9876;
+    this.server = app.listen(this.port, (err, result) => {
+      if (err) { return done(err); }
+      done();
+    });
+  });
+
+  after(() => {
+    this.server.close();
+  });
 
   it('should exist', () => {
     assert(app);
   });
+
+  describe('GET /', () => {
+    // Our tests will go here.
+  });
+
+  it('should return a 200', (done) => {
+  request.get('http://localhost:9876', (error, response) => {
+    assert.equal(response.statusCode, 200);
+    done();
+  });
+});
 
 });
